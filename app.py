5abb9b00bdd216743cf9c1535f82a329277ec584
Paul <usuikagedestiny@gmail.com>
2022-03-09 13:48:22 -0800

    update

--- app.py
+++ app.py
@@ -14,33 +14,6 @@ app = Flask(__name__)
 def index():
     return "Welcome to the server"
 
-def server():
-    #create the socket
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    # host_name = socket.gethostname()
-    # host_ip = socket.gethostbyname(host_name)
-    host_ip = 'local'
-
-    #connection output status
-    #print("Host IP:", host_ip)
-
-    #set port and socket
-    port = 9999
-    socket_address = (host_ip,port)
-
-    #binding the socket
-    server_socket.bind(socket_address)
-
-    #socket listen
-    server_socket.listen(5)
-    #print("Listening At: ",socket_address)
-
-    client_socket,addr = server_socket.accept()
-    print("Getting Connetion From:", addr)
-    
-    return "Got Connection Successful"
-
-
 
 
 
