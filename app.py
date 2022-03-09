2172bc123cc3a0e591bb7cba1f7adec862b64e97
Paul <usuikagedestiny@gmail.com>
2022-03-09 13:26:43 -0800

    update

--- app.py
+++ app.py
@@ -1,180 +1,66 @@
-from singleMD import SingleMotionDetector
-from imutils.video import VideoStream
-from flask import Response
-from flask import Flask
-from flask import render_template
-import threading
-import argparse
-import datetime
-import imutils
-import time
+# app.py
+#referenced from https://stackabuse.com/deploying-a-flask-application-to-heroku/
+import socket
 import cv2
+import pickle
+import struct
+import imutils
+from flask import Flask, request, jsonify
 
-# initialize the output frame and a lock used to ensure thread-safe
-# exchanges of the output frames (useful when multiple browsers/tabs
-# are viewing the stream)
-outputFrame = None
-lock = threading.Lock()
-# initialize a flask object
 app = Flask(__name__)
-# initialize the video stream and allow the camera sensor to
-# warmup
-#vs = VideoStream(usePiCamera=1).start()
-vs = VideoStream(src=0).start()
-time.sleep(2.0)
 
-@app.route("/")
+# A welcome message to test our server
+@app.route('/')
 def index():
-	# return the rendered template
-	return render_template("index.html")
+    return "Welcome to the server"
 
-def detect_motion(frameCount):
-	# grab global references to the video stream, output frame, and
-	# lock variables
-	global vs, outputFrame, lock
-	# initialize the motion detector and the total number of frames
-	# read thus far
-	md = SingleMotionDetector(accumWeight=0.1)
-	total = 0
+def server():
+    #create the socket
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # host_name = socket.gethostname()
+    # host_ip = socket.gethostbyname(host_name)
+    host_ip = 'local'
 
-	# loop over frames from the video stream
-	while True:
-		# read the next frame from the video stream, resize it,
-		# convert the frame to grayscale, and blur it
-		frame = vs.read()
-		frame = imutils.resize(frame, width=400)
-		gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-		gray = cv2.GaussianBlur(gray, (7, 7), 0)
-		# grab the current timestamp and draw it on the frame
-		timestamp = datetime.datetime.now()
-		cv2.putText(frame, timestamp.strftime(
-			"%A %d %B %Y %I:%M:%S%p"), (10, frame.shape[0] - 10),
-			cv2.FONT_HERSHEY_SIMPLEX, 0.35, (0, 0, 255), 1)
+    #connection output status
+    print("Host IP:", host_ip)
 
-		# if the total number of frames has reached a sufficient
-		# number to construct a reasonable background model, then
-		# continue to process the frame
-		if total > frameCount:
-			# detect motion in the image
-			motion = md.detect(gray)
-			# check to see if motion was found in the frame
-			if motion is not None:
-				# unpack the tuple and draw the box surrounding the
-				# "motion area" on the output frame
-				(thresh, (minX, minY, maxX, maxY)) = motion
-				cv2.rectangle(frame, (minX, minY), (maxX, maxY),
-					(0, 0, 255), 2)
-		
-		# update the background model and increment the total number
-		# of frames read thus far
-		md.update(gray)
-		total += 1
-		# acquire the lock, set the output frame, and release the
-		# lock
-		with lock:
-			outputFrame = frame.copy()
+    #set port and socket
+    port = 9999
+    socket_address = (host_ip,port)
 
-def generate():
-	# grab global references to the output frame and lock variables
-	global outputFrame, lock
-	# loop over frames from the output stream
-	while True:
-		# wait until the lock is acquired
-		with lock:
-			# check if the output frame is available, otherwise skip
-			# the iteration of the loop
-			if outputFrame is None:
-				continue
-			# encode the frame in JPEG format
-			(flag, encodedImage) = cv2.imencode(".jpg", outputFrame)
-			# ensure the frame was successfully encoded
-			if not flag:
-				continue
-		# yield the output frame in the byte format
-		yield(b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' + 
-			bytearray(encodedImage) + b'\r\n')
+    #binding the socket
+    server_socket.bind(socket_address)
 
-@app.route("/video_feed")
-def video_feed():
-	# return the response generated along with the specific media
-	# type (mime type)
-	return Response(generate(),
-		mimetype = "multipart/x-mixed-replace; boundary=frame")
+    #socket listen
+    server_socket.listen(5)
+    print("Listening At: ",socket_address)
 
-# check to see if this is the main thread of execution
+    try:
+        while True:
+            client_socket,addr = server_socket.accept()
+            print("Getting Connetion From:", addr)
+            #upon successful connection
+            # if client_socket:
+            #     vid = cv2.VideoCapture(0)
+            #     #while video feed is opened
+            #     while(vid.isOpened()):
+            #         img,frame = vid.read()
+            #         frame = imutils.resize(frame,width=320)
+            #         a = pickle.dumps(frame)           
+            #         message = struct.pack("Q",len(a)) + a
+            #         client_socket.sendall(message)
+            #         cv2.imshow("Transmitting Video",frame)
+            #         key = cv2.waitKey(1) & 0xFF
+
+            #         #closing socket 
+            #         if key == ord('q'):
+            #             client_socket.close()
+    except KeyboardInterrupt:
+        pass
+
+
+
+
 if __name__ == '__main__':
-	# construct the argument parser and parse command line arguments
-	ap = argparse.ArgumentParser()
-	ap.add_argument("-i", "--ip", type=str, required=True,
-		help="ip address of the device")
-	ap.add_argument("-o", "--port", type=int, required=True,
-		help="ephemeral port number of the server (1024 to 65535)")
-	ap.add_argument("-f", "--frame-count", type=int, default=32,
-		help="# of frames used to construct the background model")
-	args = vars(ap.parse_args())
-	# start a thread that will perform motion detection
-	t = threading.Thread(target=detect_motion, args=(
-		args["frame_count"],))
-	t.daemon = True
-	t.start()
-	# start the flask app
-	app.run(host=args["ip"], port=args["port"], debug=True,
-		threaded=True, use_reloader=False)
-# release the video stream pointer
-vs.stop()
-
-# # app.py
-# #referenced from https://stackabuse.com/deploying-a-flask-application-to-heroku/
-# from flask import Flask, request, jsonify
-
-# app = Flask(__name__)
-
-# @app.route('/getmsg/', methods=['GET'])
-# def respond():
-#     # Retrieve the name from url parameter
-#     name = request.args.get("name", None)
-
-#     # For debugging
-#     print(f"got name {name}")
-
-#     response = {}
-
-#     # Check if user sent a name at all
-#     if not name:
-#         response["ERROR"] = "no name found, please send a name."
-#     # Check if the user entered a number not a name
-#     elif str(name).isdigit():
-#         response["ERROR"] = "name can't be numeric."
-#     # Now the user entered a valid name
-#     else:
-#         response["MESSAGE"] = f"Welcome {name} to our awesome platform!!"
-
-#     # Return the response in json format
-#     return jsonify(response)
-
-# @app.route('/post/', methods=['POST'])
-# def post_something():
-#     param = request.form.get('name')
-#     print(param)
-#     # You can add the test cases you made in the previous function, but in our case here you are just testing the POST functionality
-#     if param:
-#         return jsonify({
-#             "Message": f"Welcome {name} to our awesome platform!!",
-#             # Add this option to distinct the POST request
-#             "METHOD" : "POST"
-#         })
-#     else:
-#         return jsonify({
-#             "ERROR": "no name found, please send a name."
-#         })
-
-# # A welcome message to test our server
-# @app.route('/')
-# def index():
-#     return "Welcome to the server"
-
-
-
-# if __name__ == '__main__':
-#     # Threaded option to enable multiple instances for multiple user access support
-#     app.run(threaded=True, port=5000)
+    # Threaded option to enable multiple instances for multiple user access support
+    app.run(threaded=True, port=5000)
